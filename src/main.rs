83a8bf516d49db9be44a52efce0cbffcd5596ed4
zaximlo <92861707+zaximlo@users.noreply.github.com>
2022-10-27 20:06:42 -0400

    brooooooooooooo
    oqxs

--- src/main.rs
+++ src/main.rs
@@ -15,17 +15,50 @@ use macroquad::color::Color;
 #[macroquad::main("idk")]
 async fn main(){
     
-    let mut img_pxls: Vec<[u8; 3]> = Vec::new();
+    let mut img_pxls: Vec<[f32; 3]> = Vec::new();
 
-    videoinput::vectorize("main.mp4", &mut img_pxls);
+    let mut W: i32 = 0;
 
-    println!("{:?}", img_pxls[5]);
+    videoinput::vectorize("main.mp4", 1, &mut img_pxls, &mut W);
+
+    let H: i32 = img_pxls.len() as i32 / W;
+
+    println!("{} {}", W, H);
+
+    // println!("{:?}", img_pxls);
 
     let black = Color::new(0.0, 0.0, 0.0, 1.);
 
-    if 1==1 {
+    loop {
         clear_background(black);
+
+        let mut index: usize = 0;
+
+        for y in 0..H {
+            for x in 0..W {
+                let pixel = img_pxls[index];
+
+                let to_col = Color::new(pixel[0], pixel[1], 0.4, 1.);
+
+                draw_rectangle(x as f32, y as f32, 1.0, 1.0, to_col);
+
+                index += 1;
+
+                // println!("{:?}", pixel);
+
+                
+                // println!("d");
+
+
+            }
+        }
+
+        let mut img_pxls: Vec<[f32; 3]> = Vec::new();
+        videoinput::vectorize("main.mp4", 290, &mut img_pxls, &mut W);
+
+
         next_frame().await
+
 
     }
 
--- src/videoinput.rs
+++ src/videoinput.rs
@@ -10,24 +10,31 @@ pub fn from_file(path: &str) -> Result<VideoCapture> {
     VideoCapture::from_file(path, CAP_ANY)
 }
 
-pub fn vectorize(filename: &str, thing: &mut Vec<[u8; 3]> ) -> Result<String>{
+pub fn vectorize(filename: &str, frame_num: i32, thing: &mut Vec<[f32; 3]>, width: &mut i32) -> Result<String>{
     let mut video = from_file("test.mp4")?;
 
     let mut times: u64 = 0;
     
     // Make img
     let mut img = Mat::default();
-    video.read(&mut img).unwrap_or(false);
 
-    let mut img_pxls: Vec<[u8; 3]> = Vec::new();
+    for _ in 0..(frame_num + 1) {
+        video.read(&mut img).unwrap_or(false);
+    }
+
+    // let mut img_pxls: Vec<[u8; 3]> = Vec::new();
 
     let size: i32 = img.rows()*img.cols();
+
+    *width = img.cols();
+
+    println!("{:?} ----------hello", img);
 
     for i in 0..img.rows() {
         for j in 0..img.cols() {
             let pixel = img.at_2d::<RGB8>(i, j).unwrap();
             
-            img_pxls.push([pixel.r, pixel.g, pixel.b]);
+            thing.push([pixel.r as f32 / 255.0, pixel.g as f32 / 255.0, pixel.b as f32 / 255.0]);
 
         }
     }
